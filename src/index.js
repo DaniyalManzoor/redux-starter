f243b37e37fb0909cda19c9ecc4493da47542deb
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-09-15 07:28:59 -0700

    subscribe and unsubscribe function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import store from "./store";
 
+//this function a store changes help us in Rerender
+const unsubscribe = store.subscribe(() => {
+  console.log("Store Change", store.getState());
+});
+
 store.dispatch({
   type: "bugAdded",
   payload: {
     description: "Bug1",
   },
 });
+
+unsubscribe();
 
 store.dispatch({
   type: "bugRemoved",
