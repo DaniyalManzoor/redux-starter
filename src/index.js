cfaae9b43caf55a9eb17cfc8c998817cd34fddec
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-09-16 22:59:36 -0700

    implement tracing in DEVTOOL

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import store from "./store";
 
 // import store from "./customStore";
-// import * as actions from "./actions";
+import * as actions from "./actions";
 
 // this function a store changes help us in Rerender
 // const unsubscribe = store.subscribe(() => {
 // console.log("Store Change", store.getState());
 // });
 
-// store.dispatch(bugAdded("Bug 1"));
+store.dispatch(actions.bugAdded("Bug 1"));
+store.dispatch(actions.bugAdded("Bug 2"));
+store.dispatch(actions.bugAdded("Bug 3"));
+store.dispatch(actions.bugResolved(1));
 
 // store.dispatch(bugResolved(1));
 
@@ -18,6 +21,7 @@ import store from "./store";
 
 // console.log(store.getState());
 
+// --------------------------------------------
 // Create new Store
 
 // store.dispatch(actions.bugAdded("Bug 1"));
