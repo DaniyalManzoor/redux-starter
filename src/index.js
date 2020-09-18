f74a528a42f07f35658f7e196577d241a64c0fa9
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-09-18 04:52:59 -0700

    add projects.js and projectAdded reducer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import configureStore from "./store/configureStore";
 
 // import store from "./customStore";
-import * as actions from "./store/bugs";
+import * as actions from "./store/projects";
+
 // * for all import is good practices only for small module
 
 const store = configureStore();
+
+store.dispatch(actions.projectAdded({ name: "Project 1" }));
 
 // this function a store changes help us in Rerender
 // const unsubscribe = store.subscribe(() => {
 // console.log("Store Change", store.getState());
 // });
 
-store.dispatch(actions.bugAdded({ description: "Bug 1" }));
-store.dispatch(actions.bugAdded({ description: "Bug 2" }));
-store.dispatch(actions.bugAdded({ description: "Bug 3" }));
-store.dispatch(actions.bugResolved({ id: 1 }));
+// store.dispatch(actions.bugAdded({ description: "Bug 1" }));
+// store.dispatch(actions.bugAdded({ description: "Bug 2" }));
+// store.dispatch(actions.bugAdded({ description: "Bug 3" }));
+// store.dispatch(actions.bugResolved({ id: 1 }));
 
 // store.dispatch(bugResolved(1));
 
