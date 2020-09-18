24089556693ac6449ac49e28a12e9d6eef3873c3
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-09-18 03:59:21 -0700

    creating slicing using redux toolkit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,36 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 // Duck Design Patterns
 // -- move into store folder
 // -- move Action types, Action Creator, reducer in single file
 // -- action creator is always export
 // -- reducer is always default export
 
-import { createAction, createReducer } from "@reduxjs/toolkit";
-
-// Action Creator
-
-export const bugAdded = createAction("bugAdded");
-export const bugRemoved = createAction("bugRemoved");
-export const bugResolved = createAction("bugResolved");
-
-// Reducers
 let lastId = 0;
 
-export default createReducer([], {
-  // key : value
-  // action: function (event => event handler)
-
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      desciprtion: action.payload.desciprtion,
-      resolved: false,
-    });
-  },
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug = bug.id === action.payload.id));
-    bugs[index].resolved = true;
-  },
-  [bugRemoved.type]: (bugs, action) => {
-    bugs.filter((bug) => bug.id !== action.payload.id);
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        desciprtion: action.payload.id,
+        resolved: false,
+      });
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
   },
 });
+
+export const { bugAdded, bugResolved } = slice.actions;
+export default slice.reducer;
