0ec8c689c8dc5206520734810f983612ab49ed95
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-09-19 06:19:04 -0700

    parameterizing middleware function log

--- src/store/middleware/logger.js
+++ src/store/middleware/logger.js
@@ -13,10 +13,9 @@
 
 // SNA like DNA haha for memorizing
 
-const logger = (store) => (next) => (action) => {
-  console.log("store", store);
-  console.log("next", next);
-  console.log("action", action);
+const logger = (param) => (store) => (next) => (action) => {
+  console.log("Logging", param);
+  next(action);
 };
 
 export default logger;
