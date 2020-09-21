7bcc0a1534a4cf047074a9f4028ceaccb7689d64
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-09-21 04:58:04 -0700

    dispatch func for API calls in redux toolkit

--- src/store/middleware/func.js
+++ src/store/middleware/func.js
@@ -1,5 +1,10 @@
+// Thunk / func
 const func = ({ dispatch, getState }) => (next) => (action) => {
   if (typeof action === "function") action(dispatch, getState);
   else next(action);
 };
 export default func;
+
+// redux has it own thunk / func
+//with out Redux toolkit download thunk
+// npm i redux-thunk
